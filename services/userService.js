6c2ca09f03fd030abfee8d9d8c14ecfcc9075f2d
Dima Chemerevsky <dchemerevsky@aras.com>
2024-02-12 16:14:48 +0100

    Implement POST request handler

--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,6 @@
-import { validate } from 'uuid';
+import { validate, v4 as createUuid } from 'uuid';
 
-const users = [{
-
-}];
+const users = [];
 
 const validateUserId = (userId) => {
 	return validate(userId);
@@ -17,4 +15,55 @@ const getUserById = (userId) => {
 	return user;
 }
 
-export { getAllUsers, validateUserId, getUserById }
+const validateUser = (userData) => {
+	const user = userData.user;
+	if (!user) {
+		return {
+			isError: true,
+			errorMessage: 'Invalid body format'
+		}
+	}
+
+	const username = user.username;
+	if (!username || typeof username !== 'string') {
+		return {
+			isError: true,
+			errorMessage: 'Please send correct username'
+		}
+	}
+
+	const age = user.age;
+	if (!age || typeof age !== 'number') {
+		return {
+			isError: true,
+			errorMessage: 'Please send correct age'
+		}
+	}
+
+	const hobbies = user.hobbies;
+	if (!hobbies || !Array.isArray(hobbies)) {
+		return {
+			isError: true,
+			errorMessage: 'Please send correct hobbies'
+		}
+	}
+
+	return {
+		isError: false
+	};
+}
+
+const createUser = (userData) => {
+	const {username, age, hobbies} = userData.user;
+	const newUser = {
+		id: createUuid(),
+		username: username,
+		age: age,
+		hobbies: hobbies
+	};
+	users.push(newUser);
+
+	return newUser;
+}
+
+export { getAllUsers, validateUserId, getUserById, validateUser, createUser }
