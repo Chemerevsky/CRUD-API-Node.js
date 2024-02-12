a0c2051e370f821c5a7e029c4820f2b742122991
Dima Chemerevsky <dchemerevsky@aras.com>
2024-02-12 17:11:23 +0100

    Handle server side errors with 500 status code

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 import http from 'node:http';
 import * as httpService from './services/httpService.js';
 
-
 const PORT = process.env.PORT || 8000;
 const server = http.createServer();
 server.on('request', async (request, response) => {
-	const responseDetails = await httpService.handleRequest(request);
-	httpService.sendResponse(responseDetails, response);
+	try {
+		const responseDetails = await httpService.handleRequest(request);
+		httpService.sendResponse(responseDetails, response);
+	} catch {
+		const serverErrorDetails = {
+			statusCode: 500,
+			headers: {},
+			data: 'Unexpected server error.'
+		};
+		httpService.sendResponse(serverErrorDetails, response);
+	}
 });
 
 server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
